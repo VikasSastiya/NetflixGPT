f5c0c3842cb2c7996a899c131d02ddc2324d8c5b
VikasSastiya <vikassastiya06@gmail.com>
2024-08-18 15:35:12 +0530

    signin/signup form && login/header

--- netflix-gpt/src/components/Header.js
+++ netflix-gpt/src/components/Header.js
@@ -1,9 +1,11 @@
 
 
 const Header=()=> {
-  return 
-    (<div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUqojpgbJT5ONrdnrxr1FeeNK2gvODsTupaw&s" alt="logo"/>
+  return  (
+    <div className="absolute px-8 py-4  z-10">
+        <img
+        className="w-44"
+        src="https://static.wixstatic.com/media/2ed9b9_514fe584406c4bcf9ab2285b120daaf0~mv2.png/v1/fill/w_225,h_66,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/GSP-colored.png" alt="logo"/>
     </div>
 );
 };
--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -1,10 +1,31 @@
 import Header from './Header';
-
+import { useState } from 'react';
 const Login = () => {
+  const [isSignInForm ,setIsSignForm]=useState(true);
+
+  const toggleSignInForm=()=>{
+    setIsSignForm(!isSignInForm);
+  };
   return (
+  
     <div>
             <Header/>
+            <div className="absolute w-full h-full" >
+            <img src="https://i.pinimg.com/originals/f2/58/14/f258140582ce9fb7dfa524cd06a8b290.jpg" alt="logo" class="w-full h-full"/>
     </div>
+    
+     <form className="w-3/12 absolute p-12 bg-yellow-700 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-40">
+      <h1 className="font-bold text-3xl py-4">{isSignInForm? "Sign In":"Sign Up"}</h1>
+      {!isSignInForm&&(<input type="text" placeholder="Full Name" className="p-4 my-2 w-full placeholder-white bg-yellow-600"/>)}
+  
+    <input type="text" placeholder="Email Address" className="p-4 my-2 w-full placeholder-white bg-yellow-600"/>
+ 
+    <input type="password" placeholder="Password" className="p-4 my-2 w-full placeholder-white bg-yellow-600"/>
+     <button className="p-4 my -6 bg-orange-700 w-full rounded-lg">{isSignInForm? "Sign In":"Sign Up"}</button>
+     <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>{isSignInForm? "New to Globel School? SignUp Now":"Already registered? SignUp now"}</p>
+    </form>
+    </div>
+   
   );
 };
 
